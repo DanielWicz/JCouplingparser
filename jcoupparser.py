27cddc0b62b78b00f3ee9caff2dbe28c7befcdaa
Daniel Wiczew <daniel.wiczew@gmail.com>
2021-12-20 17:52:05 +0100

    Format according to PEP8

--- jcoupparser.py
+++ jcoupparser.py
@@ -1,6 +1,7 @@
 import csv
 import re
 
+
 class JCoupFileMaker:
     """A class that's pruporse is to create tbl J coupling files from assigned
        J coupling signal. The assigment is saved into a csv file with a format shown
@@ -9,10 +10,10 @@ class JCoupFileMaker:
            NH1,4.23
            NH2,6.27
            NH3,2.52
-       To use the class you've to create its object instance with 
-       two arguments `jcoup_fname` and `jcoupwrite_fname` as in 
+       To use the class you've to create its object instance with
+       two arguments `jcoup_fname` and `jcoupwrite_fname` as in
        the example below:
-       
+
        ```python
        from JCouplingparser import JCoupFileMaker
 
@@ -29,9 +30,11 @@ class JCoupFileMaker:
     """
 
     def __init__(self, jcoupcsv_fname=None, jcoupwrite_fname=None):
-        self.basestring = 'assign (resid   {0} and name hn )   (resid  {0} and name n ) '+\
-                          '(resid   {1} and name ca)   (resid  {1} and name ha )  {2} 0.5'
-        self.csv_regexp = '([A-Z]+)([0-9]+)\ *'
+        self.basestring = (
+            "assign (resid   {0} and name hn )   (resid  {0} and name n ) "
+            + "(resid   {1} and name ca)   (resid  {1} and name ha )  {2} 0.5"
+        )
+        self.csv_regexp = "([A-Z]+)([0-9]+)\ *"
         self.regexp_comp = re.compile(self.csv_regexp)
         self.read_file_lines = []
         self.jcoupcsv_fname = jcoupcsv_fname
@@ -39,11 +42,11 @@ class JCoupFileMaker:
 
     def read_jcoupcsv(self):
         """
-        A method that, allows for reading specified CSV file with 
+        A method that, allows for reading specified CSV file with
         J coupling constants. The structure of the file is specified
         in the class's docstring.
         """
-        with open(self.jcoupcsv_fname, 'r') as csvfile:
+        with open(self.jcoupcsv_fname, "r") as csvfile:
             reader = csv.DictReader(csvfile)
             for i, row in enumerate(reader):
                 keys = list(row.keys())
@@ -51,25 +54,29 @@ class JCoupFileMaker:
                 matched = self.regexp_comp.match(row[keys[0]])
                 # group(1) is aminoacid atom name, group(2) is resiude number
                 # strength is j-coupling constant
-                self.read_file_lines.append((matched.group(1), int(matched.group(2)), float(strength)))
-
+                self.read_file_lines.append(
+                    (matched.group(1), int(matched.group(2)), float(strength))
+                )
 
     def save_jcoup_file(self):
         """
         A method that saves tbl file with given J coupling constants
         """
-        with open(self.jcoupwrite_fname, 'w') as f:
+        with open(self.jcoupwrite_fname, "w") as f:
             read_file_lines_len = len(self.read_file_lines)
             for i, line in enumerate(self.read_file_lines):
                 atom, resid, strength = line
                 if resid == 1:
                     continue
-                to_write = self.basestring.format(resid-1, resid, strength)
+                to_write = self.basestring.format(resid - 1, resid, strength)
                 if i < read_file_lines_len:
-                    to_write += '\n'
+                    to_write += "\n"
                 f.write(to_write)
 
-if __name__ == '__main__':
-    jcoupmaker = JCoupFileMaker(jcoupcsv_fname='jcoup.csv', jcoupwrite_fname='jcoup.tbl')
+
+if __name__ == "__main__":
+    jcoupmaker = JCoupFileMaker(
+        jcoupcsv_fname="jcoup.csv", jcoupwrite_fname="jcoup.tbl"
+    )
     jcoupmaker.read_jcoupcsv()
     jcoupmaker.save_jcoup_file()
